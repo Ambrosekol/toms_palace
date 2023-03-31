a4390896a64450b47fc4e1aae17aed78d086b447
Impartial_Raven <kolus7381@gmail.com>
2023-03-31 14:52:28 +0100

    get menu items from database (unfinished)

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_indicator/loading_indicator.dart';
+import 'package:toms_palace/models/menu.dart';
 import 'package:toms_palace/models/user.dart';
 import 'package:toms_palace/util/firebaseinstance.dart';
 
@@ -14,11 +15,15 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   AppUser appUser = AppUser();
   late Stream<DocumentSnapshot<Map<String, dynamic>>> userdata;
+  late Stream<QuerySnapshot<Map<String, dynamic>>> foodMenuGetter;
+  late List<FoodMenu> foodmenu;
 
   @override
   void initState() {
     userdata = appUser.getuserData();
-    // foodMenu = db.collection('Food Menu').snapshots().
+    //GET MENU ITEMS
+    // foodMenuGetter = db.collection('Food Menu').snapshots();
+    // foodMenuGetter.forEach((element) {element.docs});
     super.initState();
   }
 
@@ -28,26 +33,7 @@ class _HomescreenState extends State<Homescreen> {
       stream: userdata,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return Scaffold(
-            body: Center(
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: LoadingIndicator(
-                  strokeWidth: 10,
-                  // pathBackgroundColor: Colors.red,
-                  colors: [
-                    Colors.teal,
-                    Colors.tealAccent,
-                    Theme.of(context).primaryColor,
-                    Theme.of(context).secondaryHeaderColor,
-                  ],
-                  indicatorType: Indicator.ballSpinFadeLoader,
-                  // backgroundColor: Theme.of(context).primaryColor,
-                ),
-              ),
-            ),
-          );
+          return Scaffold();
         } else {
           return Scaffold(
             body: Center(
